3b1ed32d9110117a80b35cc5f00052b693983dca
Ritchie Swann <ritchieswann@gmail.com>
2023-10-27 09:39:36 +0100

    don't run sna2skool

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 rm -rf hunch
-sna2skool.py -H -c sources/hunch.ctl sources/hunch.z80 -e 61000 > sources/hunch.skool
+#sna2skool.py -H -c sources/hunch.ctl sources/hunch.z80 -e 61000 > sources/hunch.skool
 skool2html.py -H sources/hunch.skool
